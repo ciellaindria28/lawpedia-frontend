18f440b23893ac7a4fdb80413deed3c897a65c5f
Graciella Regina <ciellaindria28@gmail.com>
2023-05-01 00:34:15 +0700

    Frontend implementation

---
+++ frontend/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def login_view(request):
+    return render(request, 'login.html')
+
+
+def add_product(request):
+    return render(request, 'productform.html')
+
+def product_list(request):
+    return render(request, 'productlist.html')
+
+def cart_view(request):
+    return render(request, 'cart_view.html')
+
+def order_list(request):
+    return render(request, 'order_list.html')
+
+def order_detail(request):
+    return render(request, 'order_detail.html')
--- lawpedia/urls.py
+++ lawpedia/urls.py
@@ -15,9 +15,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,re_path
+
+# drf_yasg code starts here
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Lawpedia API",
+        default_version='v1',
+        description="Welcome to the Lawpedia API",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+# ends here
 
 urlpatterns = [
+    re_path(r'^doc(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),  
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),  
     path('admin/', admin.site.urls),
-    path('orders/', include('orders.urls'))
+    path('orders/', include('orders.urls')),
+    path('', include('frontend.urls'))
 ]
