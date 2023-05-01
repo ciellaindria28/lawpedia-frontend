d1ccbdb0ad3a5a598551283337704b8a759e7a1a
Graciella Regina <ciellaindria28@gmail.com>
2023-05-01 21:32:13 +0700

    Add requirements and fix url naming

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,9 +5,9 @@ app_name = "frontend"
 
 urlpatterns = [
     path('login', login_view, name='login'),
-    path('addproduct', add_product, name='addproduct'),
-    path('product_list', product_list, name='product_list'),
-    path('cart', cart_view, name='cart'),
-    path('order_list', order_list, name='order_list'),
+    path('products/create', add_product, name='addproduct'),
+    path('products', product_list, name='product_list'),
+    path('cart/all', cart_view, name='cart'),
+    path('orders', order_list, name='order_list'),
 
 ]
