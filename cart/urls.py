b9cf7a9667434bbc72b601749754d09adf9041d2
Graciella Regina <ciellaindria28@gmail.com>
2023-05-20 00:40:03 +0700

    Add implementation for read, update,delete for cart

--- cart/urls.py
+++ cart/urls.py
@@ -6,4 +6,9 @@ app_name = "cart"
 urlpatterns = [
     path('<int:item_id>', get_cart_products_by_id, name='get_cart_products_by_id'),
     path('add', add_product_to_cart, name='add_product_to_cart'),
+    path('', get_all_carts, name='get_all_carts'),
+    path('update-quantity', update_cart_quantity, name='update_cart_quantity'),
+    path('delete', delete_cartproduct, name='delete_cartproduct'),
 ]
+
+#Delete cart
--- cart/views.py
+++ cart/views.py
@@ -15,31 +15,45 @@ import json
 LAWPEDIA_AUTH = 'http://localhost:8001/auth/user'
 LAWPEDIA_PRODUCT = 'http://localhost:3000/products'
 
+#Get cart based on id
 @api_view(['GET'])
-def get_cart_products_by_id(request, item_id):
-    #TODO: Return data with format like dummy
-    if item_id == 1:
-        data = {"cartProductId": 1, 
-                "productId": 1, 
-                "quantity": 3,  
-                "buyerUsername": "alice", 
-                "sellerUsername": "trudy", 
-                } 
-    elif item_id == 2:
-        data = {"cartProductId": 2, 
-                "productId": 2, 
-                "quantity": 2,  
-                "buyerUsername": "alice", 
-                "sellerUsername": "trudy", 
-                } 
-    else :
-        data = {"cartProductId": 3, 
-                "productId": 3, 
-                "quantity": 4,  
-                "buyerUsername": "alice", 
-                "sellerUsername": "trudy", 
-                }
-    return Response(data=data, status=status.HTTP_200_OK)
+def get_cart_products_by_id(request,item_id):
+    try:
+        token = request.META.get('HTTP_AUTHORIZATION').split(' ')[1]
+    except:
+        return Response(data={"error": "No token provided."}, status=status.HTTP_401_UNAUTHORIZED)
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(LAWPEDIA_AUTH, headers=headers)
+    if response.status_code == 200:
+        try:
+            cart = Cart.objects.get(cartProductId=item_id)
+            serialized_cart = CartProductSerializer(cart).data
+            return Response(data=serialized_cart, status=status.HTTP_200_OK)
+        except Cart.DoesNotExist:
+            return Response(data={"error": "Cart with the provided id does not exist."}, status=status.HTTP_404_NOT_FOUND)
+    else:
+        return Response(data={"error": "Token not valid."}, status=status.HTTP_401_UNAUTHORIZED)
+
+#Get all carts
+@api_view(['GET'])
+def get_all_carts(request):
+    try:
+        token = request.META.get('HTTP_AUTHORIZATION').split(' ')[1]
+    except:
+        return Response(data={"error": "No token provided."}, status=status.HTTP_401_UNAUTHORIZED)
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(LAWPEDIA_AUTH, headers=headers)
+    if response.status_code == 200:
+        response_body = json.loads(response.text)
+        username = response_body['data']['username']
+        carts = Cart.objects.filter(buyerUsername=username)
+        carts_serialized = []
+        for cart in carts:
+            serialized_cart = CartProductSerializer(cart).data
+            carts_serialized.append(serialized_cart)
+        return Response(data=carts_serialized, status= status.HTTP_200_OK)
+    else:
+        return Response(data={"error": "Token not valid."}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 #Add product to cart
@@ -87,8 +101,9 @@ def add_product_to_cart(request):
     
     else:
         return Response(data={"error": "Insufficient product quantity."}, status=status.HTTP_400_BAD_REQUEST)
-    
 
+
+#function to check product stock
 @api_view(['GET'])
 def check_seller_product_stock_by_id(request, item_id, token):
     
@@ -97,4 +112,80 @@ def check_seller_product_stock_by_id(request, item_id, token):
     
     data = json.loads(response.text)
     return Response(data=data, status=status.HTTP_200_OK)
+
+    
+#Update quantity
+@api_view(['PUT'])
+def update_cart_quantity(request):
+    try:
+        token = request.META.get('HTTP_AUTHORIZATION').split(' ')[1]
+    except:
+        return Response(data={"error": "No token provided."}, status=status.HTTP_401_UNAUTHORIZED)
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(LAWPEDIA_AUTH, headers=headers)
+    token_username = ''
+    if response.status_code == 200:
+        response_body = json.loads(response.text)
+        token_username = response_body['data']['username']
+    else:
+        return Response(data={"error": "Token not valid."}, status=status.HTTP_401_UNAUTHORIZED)
+    
+    cartId = request.data.get('cartId')
+    quantity = request.data.get('quantity')
+
+    try:
+        cart = Cart.objects.get(cartProductId=cartId)
+        productId = cart.productId
+    except Cart.DoesNotExist:
+        return Response(data={"error": "Cart with the provided cartId does not exist."}, status=status.HTTP_404_NOT_FOUND)
+    
+    try:
+        # Check product stock using the external function
+        sub_request = HttpRequest()
+        sub_request.method = 'GET'
+        response = check_seller_product_stock_by_id(sub_request, productId, token)
+        data = response.data
+        product_quantity = data['stock']
+
+        # Check if stock satisfies the quantity 
+        if product_quantity >= quantity:
+            # Update the stock of the cart
+            cart.quantity = quantity
+            cart.save()
+            
+            # Prepare the JSON response
+            cart_product_serialized = CartProductSerializer(cart).data
+                
+            # Return the JSON response
+            return Response(data=cart_product_serialized, status=status.HTTP_200_OK)
+        
+        else:
+            return Response(data={"error": "Insufficient product quantity."}, status=status.HTTP_400_BAD_REQUEST)
+    
+    except requests.exceptions.RequestException:
+        return Response(data={"error": "Failed to connect to the external API."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+#Delete cart
+@api_view(['DELETE'])
+def delete_cartproduct(request):
+    try:
+        token = request.META.get('HTTP_AUTHORIZATION').split(' ')[1]
+    except:
+        return Response(data={"error": "No token provided."}, status=status.HTTP_401_UNAUTHORIZED)
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(LAWPEDIA_AUTH, headers=headers)
+    if response.status_code == 200:
+        cartId = request.data['cartId']
+        try:
+            cart = Cart.objects.get(cartProductId=cartId)
+            cart.delete()
+            return Response(data={"message": "Cart successfully deleted."}, status=status.HTTP_204_NO_CONTENT)
+        except Cart.DoesNotExist:
+            return Response(data={"error": "Cart with the provided id does not exist."}, status=status.HTTP_404_NOT_FOUND)
+    else:
+        return Response(data={"error": "Token not valid."}, status=status.HTTP_401_UNAUTHORIZED)
+   
+
+
 
